d02409bf5a71ba47a89218df3e33ccb9b5b28a7f
Mateo Navarrete <mateonavarrete@pursuit.org>
2020-12-14 16:41:02 -0500

    update js

--- force-app/main/default/lwc/customFileUploader/customFileUploader.js
+++ force-app/main/default/lwc/customFileUploader/customFileUploader.js
@@ -1,14 +1,15 @@
 import { LightningElement, api, track } from "lwc";
+import { FlowAttributeChangeEvent } from "lightning/flowSupport";
 
 export default class CustomFileUploader extends LightningElement {
-  @api contentDocumentIDs;
+  @api contentDocumentIDs = [];
   @api flexipageRegionWidth;
   @api formats =
     ".csv, .doc, .docx, .gif, .jpeg, .jpg, .pdf, .png, .rtf, .txt, .word, .xls, .xlsx, .zip";
   @api label = "File Uploader";
   @api multipleFiles = false;
   @api recordId;
-  @api uploadedFileNames;
+  @api uploadedFileNames = [];
 
   @track uploadedFiles = [];
 
@@ -40,6 +41,15 @@ export default class CustomFileUploader extends LightningElement {
         console.log("@uFNs", this.uploadedFileNames);
         console.log("@rID", this.recordId);
       }
+      files.forEach((file) => {
+        console.log("Uploaded => ", file.documentId, file.name);
+        this.contentDocumentIDs.push(file.documentId);
+        this.uploadedFileNames.push(file.name);
+      });
+      console.log("@e", e, files);
+      ["contentDocumentIDs", "uploadedFileNames", "recordId"].forEach((prop) =>
+        this.dispatchEvent(new FlowAttributeChangeEvent(prop, this[prop]))
+      );
     } catch (err) {
       console.error(err);
     }
